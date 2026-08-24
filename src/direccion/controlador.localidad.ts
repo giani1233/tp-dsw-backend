9bafd21b1792c8215e3fdb11f6b63a11af1428f6
giani1233 <gianimesapelle04@gmail.com>
2026-08-24 17:04:34 -0300

    Cambio para prueba

--- src/direccion/controlador.localidad.ts
+++ src/direccion/controlador.localidad.ts
@@ -7,6 +7,7 @@ const em = orm.em
 
 function sanitizeLocalidadInput(req: Request, res: Response, next: NextFunction){
     const schema = req.method === 'POST' ? LocalidadSchema : LocalidadSchema.partial()
+    console.log('BODY LOCALIDAD:', req.body)
     const result = schema.safeParse(req.body)
     if (!result.success) {
         return res.status(400).json({ message: 'Datos inválidos', errors: result.error.flatten().fieldErrors })
